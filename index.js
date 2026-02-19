d01f07c9c47d06908a2d21fa8423f534d56630b5
shuvo2o <eng.shuvo03@gmail.com>
2026-02-19 13:19:22 +0600

    routing part1

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express()
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
-app.listen(port, ()=>{
+app.get('/about', (req, res)=>{
+    res.send('This is about page')
+})
+app.get('/contact', (req, res)=>{
+    res.send('This is contact page')
+})
+
+app.listen(port, () => {
     console.log(`Example app listening port ${port}`)
 })
