1dca39bf657ac0f94737882268b164c6841dbcb7
shuvo2o <eng.shuvo03@gmail.com>
2026-02-20 10:28:29 +0600

    routing part part 2

--- index.js
+++ index.js
@@ -6,11 +6,26 @@ app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
-app.get('/about', (req, res)=>{
+app.get('/about', (req, res) => {
     res.send('This is about page')
 })
-app.get('/contact', (req, res)=>{
+app.get('/contact', (req, res) => {
     res.send('This is contact page')
+})
+
+app.put('/edit-post/:id', (req, res) => {
+    console.log("Editing Put request");
+    res.send("Editing Post Successful")
+})
+app.patch('/edit-post/:id', (req, res) => {
+    console.log("Editing Patch request");
+    res.send("Editing Patch Successful")
+})
+app.delete('/post/:id', (req, res) => {
+    const postId= req.params.id;
+    console.log("Delete request");
+    console.log(postId);
+    res.send("Deleting Successful")
 })
 
 app.listen(port, () => {
